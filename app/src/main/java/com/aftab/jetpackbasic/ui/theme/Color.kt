9589d173ac8dccb527e2e44140abe80d7250a14d
rjaftab786 <aftab.thechamp786@gmail.com>
2022-09-04 22:09:06 +0530

    Initial commit

---
+++ app/src/main/java/com/aftab/jetpackbasic/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.aftab.jetpackbasic.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val Navy = Color(0xff073042)
+val Blue = Color(0xff4285f4)
+val LightBlue = Color(0xffd7effe)
+val Chartreuse = Color(0xffeff7cf)
